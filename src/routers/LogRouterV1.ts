44dab10d7457389a738c6408432110e01fab4cd5
Lonmee <Lonmee@126.com>
2021-10-01 10:28:22 +0800

    xx

--- src/routers/LogRouterV1.ts
+++ src/routers/LogRouterV1.ts
@@ -1,5 +1,6 @@
 import Router from "koa-router";
-import {logInfo} from "../Utils";
+import {logInfo, parsePostData} from "../Utils";
+import {Mongo} from "../db/Mongo";
 
 const router = new Router()
 router.all(['/'],
@@ -7,10 +8,22 @@ router.all(['/'],
         let query = '';
         switch (context.method) {
             case 'POST':
-                // todo: login with account & password
+                const users = Mongo.collections[Mongo.COLLECTIONS_KEY.users];
+                const postData: any = await parsePostData(context);
+                const findResult: any = await users.find({name: postData.name});
+                if (findResult) {
+                    if (postData.pin === findResult.pin) {
+                        context.session!.id = findResult._id;
+                        context.body = findResult;
+                    } else {
+                        context.body = {code: 203, msg: 'invalid password'};
+                    }
+                } else {
+                    context.body = {code: 204, msg: 'invalid account'};
+                }
                 break;
             case 'DELETE':
-                // todo: logout
+                context.session!.maxAge = 0
                 break;
             default:
                 logInfo(context);
