4fe8ff87ca331eae257b935ff66381ab9b56ad08
Lonmee <Lonmee@126.com>
2021-10-02 23:21:27 +0800

    op-many added

--- src/db/Mongo.ts
+++ src/db/Mongo.ts
@@ -1,6 +1,5 @@
-import {Collection, MongoClient, ObjectId} from "mongodb";
+import {Collection, MongoClient} from "mongodb";
 import {DB_CONFIG} from "../Config";
-import {parsePostData} from "../Utils";
 
 const
     client = new MongoClient(DB_CONFIG.mongodb.url);
@@ -57,58 +56,24 @@ export function mongoOP({key, type, filter = {}, postData = {}}: MongoOP) {
     switch (type) {
         case OP_TYPE.c:
             return col.insertOne({...postData});
+        case OP_TYPE.cm:
+            return col.insertMany({...postData});
         case OP_TYPE.r:
             return col.findOne(filter);
+        case OP_TYPE.rm:
+            return col.find(filter);
         case OP_TYPE.u:
             return col.updateOne(filter, {$set: {...postData}});
+        case OP_TYPE.um:
+            return col.updateMany(filter, {$set: {...postData}});
         case OP_TYPE.d:
             return col.deleteOne(filter);
+        case OP_TYPE.dm:
+            return col.deleteMany(filter);
         case OP_TYPE.i:
-            // return col.indexes(COLLECTIONS_KEY[key]);
+            return col.createIndex(filter);
             break;
     }
 }
 
 export const Mongo = {COLLECTIONS_KEY, OP_TYPE, setup}
-
-/**
- * format: "mongodb://user:password@localhost:27017/dbname"
- */
-// const cs = new ConnectionString('mongodb://localhost:1234');
-// cs.searchParams.set('readPreference', 'secondary');
-// console.log(cs.href);
-
-/**
- * op
- */
-// try {
-//     /* insert one */
-//     // const insertResult = await collection.insertOne({_id: new ObjectId("123456654321")});
-//     // console.log('Inserted documents =>', insertResult);
-// } catch (error) {
-//     if (error instanceof MongoServerError) {
-//         console.log(`Error worth logging: ${error}`);
-//     }
-//     throw error;
-// }
-/* insert many */
-// const insertResult = await collection.insertMany([{a: 1}, {a: 2}, {a: 3}]);
-// console.log('Inserted documents =>', insertResult);
-/* update one */
-// const updateOneResult = await collection.updateOne({b: 88}, {$set: {c: 88}});
-// console.log('Updated documents =>', updateOneResult);
-/* update many */
-// const updateManyResult = await collection.updateMany({b: 2}, {$set: {b: 88}});
-// console.log('Updated documents =>', updateManyResult);
-/* delete */
-// const deleteResult = await collection.deleteMany({b: 88});
-// console.log('Deleted documents =>', deleteResult);
-/* index */
-// const indexName = await collection.createIndex({a: 1});
-// console.log('index name =', indexName);
-/* find all */
-// const findResult = await collection.find({}).toArray();
-// console.log('Found documents =>', findResult);
-/* find with query filter */
-// const filteredDocs = await collection.find({ a: 3 }).toArray();
-// console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
